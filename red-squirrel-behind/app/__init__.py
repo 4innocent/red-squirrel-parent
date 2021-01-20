a0efa5d1edc4e98cc82a0931223bd528f36a23a5
4innocent <gong_kangjia@163.com>
2021-01-21 01:48:45 +0800

    init

--- red-squirrel-behind/app/__init__.py
+++ red-squirrel-behind/app/__init__.py
@@ -2,13 +2,20 @@ from conf import Conf
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     """ 创建app """
     app = Flask(__name__)
     app.config.from_object(Conf)
+    # 解决cors跨域问题
     CORS(app)
+    # 注册sqlalchemy管理数据库
+    db.init_app(app)
+    # migreate
+    migrate.init_app(app)
     return app
---
+++ red-squirrel-behind/app/api/__init__.py
@@ -0,0 +1,3 @@
+from flask import blueprints
+
+api = blueprints.Blueprint("api", __name__)
